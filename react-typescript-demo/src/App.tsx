60ad93c5bf9f3fc067fe6dd161acc91d57e96e6f
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2022-02-20 19:11:49 +0900

    relocate components

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import { Greet } from './components/Greet';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
-import { Status } from './Status';
+import { Status } from './components/Status';
 
 function App() {
   const personName = {
