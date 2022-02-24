732b287de2e608a16dcd360e8da8254ac5676189
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2022-02-25 00:50:25 +0900

    event props in typescript

---
+++ react-typescript-demo/src/components/Input.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+type InputProps = {
+  value: string
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export const Input = (props: InputProps ) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => console.log(event)
+  return <input type='text' value={props.value} onChange={handleInputChange}/> 
+  // need at least 2 props  - input value , onChange handler
+}
