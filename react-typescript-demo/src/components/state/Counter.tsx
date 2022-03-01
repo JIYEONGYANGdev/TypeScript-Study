ec1ecaf6d1ef2fde7f76afe6525a37898879218c
JIYEONGYANGdev <sootulliyang.dev@gmail.com>
2022-03-01 21:25:12 +0900

    useReducer strict Action type in typescript by discriminated union

--- react-typescript-demo/src/components/state/Counter.tsx
+++ react-typescript-demo/src/components/state/Counter.tsx
@@ -4,9 +4,15 @@ type CounterState = {
   count: number
 }
 
-type CounterAction = {
-  type: string
+type CounterAction = UpdateAction | ResetAction
+
+type UpdateAction = {
+  type: 'increment' | 'decrement'
   payload: number
+}
+
+type ResetAction = {
+  type: 'reset'
 }
 
 const initialState = {count: 0}
@@ -17,6 +23,8 @@ function reducer (state: CounterState , action: CounterAction) {
       return {count: state.count + action.payload}
     case 'decrement':
       return {count: state.count - action.payload}
+    case 'reset':
+      return initialState
     default: 
       return state 
   }
@@ -30,7 +38,8 @@ export const Counter = () => {
     Count: {state?.count}
     <br/>
     <button onClick={() => dispatch({type: 'increment', payload: 10})}>Increment 10</button>
-  <button onClick={() =>  dispatch({type: 'decrement', payload: 10})}>Decrement 10</button>
+    <button onClick={() =>  dispatch({type: 'decrement', payload: 10})}>Decrement 10</button>
+    <button onClick={() =>  dispatch({type: 'reset'})}>Reset</button>
     </>
   )
 }
